d8c96dd2941dd6ae16fc85b6752d7c468771a3df
matteoferla <matteo.ferla@gmail.com>
2022-05-07 17:25:44 +0100

    :truck: added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='jsme_hack',
+    version='0.1',
+    packages=['jsme_hack'],
+    url='https://github.com/matteoferla/JSME_notebook_hack',
+    license='MIT',
+    author='Matteo Ferla',
+    author_email='matteo.ferla@gmail.com',
+    description='JSME molecular editor in a Jupyter or Colab notebook in a hacky way.'
+)
